3d8876972ae23c476b9d11cf23d5a28ee4e0a5f8
Ashish Rai <ashishraics512@gmail.com>
2021-11-02 16:31:03 +0530

    Add Frontend Build

---
+++ build/lib/streamlit_text_like/frontend/build/precache-manifest.7c10d738c2386906ebb5494bead30c22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff8fa2208f64ecbf87cf55c809c00e17",
+    "url": "./index.html"
+  },
+  {
+    "revision": "668e8dde3f0c42adb1a4",
+    "url": "./static/js/2.6144663e.chunk.js"
+  },
+  {
+    "revision": "5e47f1bc70bf90fbe0437e0e7f9187a9",
+    "url": "./static/js/2.6144663e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fce6a1199688cef6328",
+    "url": "./static/js/main.73a72572.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
